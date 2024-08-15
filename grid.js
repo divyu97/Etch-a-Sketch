c4b08bd7cc9a73d3908dfd93f097a6763f130b2d
Divyu <divyendureddy@gmail.com>
2024-08-16 00:06:41 +0530

    Fix createRainbowSquares function

--- grid.js
+++ grid.js
@@ -27,16 +27,18 @@ function resetSquares() {
 
 function createRainbowSquares() {
     const squares = document.querySelectorAll(".grid-square");
-    grid.removeChild(squares);
+    for (const squre of squares) {
+        grid.removeChild(squre);
+    }
     for (let i = 0; i < gridSize ** 2; i++) {
         const square = document.createElement("div");
         square.setAttribute("class", "grid-square");
-        square.style.cssText = `width: ${500 / n}px; height: ${500 / n}px`;
+        square.style.cssText = `width: ${500 / gridSize}px; height: ${500 / gridSize}px`;
         grid.appendChild(square);
         square.addEventListener("mouseover", () => {
-            const r = Math.round(Math.random() * 100);
-            const g = Math.round(Math.random() * 100);
-            const b = Math.round(Math.random() * 100);
+            const r = Math.round(Math.random() * 255);
+            const g = Math.round(Math.random() * 255);
+            const b = Math.round(Math.random() * 255);
             square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         });
     }   
