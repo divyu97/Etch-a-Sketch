706055a23c3c6b3f7b5acd222ab7378d5d1b812b
Divyu <divyendureddy@gmail.com>
2024-08-15 22:49:54 +0530

    Create a function to make rainbow type grid squares

--- grid.js
+++ grid.js
@@ -23,6 +23,24 @@ function resetSquares() {
     }
 }
 
+function createRainbowSquares() {
+    const grid = document.querySelector(".grid-container");
+    const squares = document.querySelectorAll(".grid-square");
+    grid.removeChild(squares);
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "grid-square");
+        square.style.cssText = `width: ${500 / n}px; height: ${500 / n}px`;
+        grid.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            const r = Math.round(Math.random() * 100);
+            const g = Math.round(Math.random() * 100);
+            const b = Math.round(Math.random() * 100);
+            square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        });
+    }   
+}
+
 document.querySelector(".reset").addEventListener("click", resetSquares);
 
 createSquares(gridSize);
